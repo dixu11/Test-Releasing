d0714134293fbdefed01e7971c19e079c1bb9a6a
Marcin Sowa <marcin.sowa121@gmail.com>
2020-02-23 14:43:31 +0100

    test release2

---
+++ src/main/java/org/example/my/marsby/test/Trololo.java
@@ -0,0 +1,11 @@
+package org.example.my.marsby.test;
+
+import java.util.Random;
+
+public class Trololo {
+
+    public void testMe() {
+        System.out.println("Nuberooo.");
+        System.out.println(new Random().nextDouble());
+    }
+}
